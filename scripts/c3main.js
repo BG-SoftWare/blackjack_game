e90ee0f0771bd8cbfe9e22a63e45dc532a648bf8
Danylo Yeromenko <danylo.yeromenko@bgsoftware.co>
2025-10-24 18:12:39 +0300

    fix

--- scripts/c3main.js
+++ scripts/c3main.js
@@ -50121,7 +50121,7 @@ void main(void) {
 
 function loadTutorial() {
     const e = parseInt(localStorage.getItem("tutorial"), 10);
-    runtime.globalVars.tutorial = isNaN(e) ?  null : e
+    runtime.globalVars.tutorial = isNaN(e) ?  0 : e
 }
 
 function setTutorial(e) {
